2f357fa57086cf8373008725a648a5238c185901
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-03-31 23:48:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         MyList<String> myArrayList = new MyArrayList<>();
+        System.out.println(myArrayList);
         myArrayList.add("Привет");
         myArrayList.add("Я джун");
         myArrayList.add("Пока");
